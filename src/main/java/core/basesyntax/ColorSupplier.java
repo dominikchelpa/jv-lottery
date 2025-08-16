2856895960585e6aba1cd40858db64a2e884935c
Dominik Chelpa <dmnc.ch@gmail.com>
2025-08-16 09:57:43 +0200

    Solution

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,12 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
-    public String getRandomColor() {
-        return null;
+    private Random random = new Random();
+
+    public Ball.Color getRandomColor() {
+        Ball.Color[] colors = Ball.Color.values();
+        return colors[random.nextInt(colors.length)];
     }
 }
